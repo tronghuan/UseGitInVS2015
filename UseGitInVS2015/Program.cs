f90a8b2072661d12df9fafe679e1f5ae560673e9
tronghuan <huandt92@gmail.com>
2018-05-30 09:42:31 +0900

    new code in main function

--- UseGitInVS2015/Program.cs
+++ UseGitInVS2015/Program.cs
@@ -11,6 +11,7 @@ namespace UseGitInVS2015
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.Write("Programming with C#");
         }
     }
 }
